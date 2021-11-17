bc3788c813b84c27545d609b1bacc755afc3959f
Akshat Sethi <masterakshata@gmail.com>
2021-11-17 13:24:27 +0530

    init

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/App.css';
+const App = () => {
+  const [marco,polo] = useState(true)
+  return (
+    <div id="main">
+
+    </div>
+  )
+}
+
+
+export default App;
